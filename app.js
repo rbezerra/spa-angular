aea3441ab36e2c6735aaecaddbb434013b0158f5
Ramon Bezerra <ramonbezerra90@gmail.com>
2016-12-02 15:09:38 -0300

    adicionando service

--- app.js
+++ app.js
@@ -1,39 +1,101 @@
-var app = angular.module('spaExemplo',['ngRoute']);
+var app = angular.module('spaExemplo', ['ngRoute']);
 
-app.config(function($routeProvider){
+app.config(function ($routeProvider) {
 	$routeProvider
 
-	.when('/', {
-		templateUrl : 'paginas/home.html',
-		controller : 'mainController'
-	})
+		.when('/', {
+			templateUrl: 'paginas/home.html',
+			controller: 'mainController'
+		})
 
-	.when('/sobre', {
-		templateUrl : 'paginas/sobre.html',
-		controller : 'sobreController'
-	})
+		.when('/sobre', {
+			templateUrl: 'paginas/sobre.html',
+			controller: 'sobreController'
+		})
 
-	.when('/contato', {
-		templateUrl : 'paginas/contato.html',
-		controller : 'contatoController'
-	});
+		.when('/contato', {
+			templateUrl: 'paginas/contato.html',
+			controller: 'contatoController'
+		})
+		.when('/tarefas', {
+			templateUrl: 'paginas/tarefas.html',
+			controller: 'tarefasController'
+		});
 });
 
-app.controller('mainController', function($scope){
+app.controller('mainController', function ($scope) {
 
 	$scope.message = 'Hello World!!!';
 
 });
 
-app.controller('sobreController', function($scope){
+app.controller('sobreController', function ($scope) {
 
 	$scope.message = 'AngularJS é um framework JavaScript MV* para aplicações Web!!!';
 
 });
 
-app.controller('contatoController', function($scope){
+app.controller('contatoController', function ($scope) {
 
 	$scope.message = 'Entre em contato conosco em angular@js.com';
 
 });
+
+app.controller('tarefasController', function ($scope, TarefasService) {
+	$scope.tarefas = TarefasService.getAll();
+
+	$scope.addTarefa = function(){
+		var novaTarefa = {
+			descricao: $scope.formTarefaTexto,
+			done:false
+		};
+
+		TarefasService.addTarefa(novaTarefa);
+
+		$scope.formTarefaTexto = "";
+
+	};
+
+	$scope.apagarTarefa = function(tarefa){
+		TarefasService.removeTarefa(tarefa);
+	}
+
+});
+
+app.service('TarefasService', function () {
+	var tarefas = [
+		{
+			descricao:'Aprender AngularJS',
+			done:true
+		},
+		{
+			descricao:'Comprar Cerveja',
+			done: false
+		}
+	];
+
+	return {
+		getAll: function () {
+			return tarefas;
+		},
+		getById: function (id) {
+			return tarefas.find(function (element, index, array) {
+				return element.id === id;
+			})
+		},
+		addTarefa: function (tarefa) {
+			tarefa.done = false;
+			tarefas.push(tarefa);
+		},
+		doneTarefa: function (tarefa) {
+			var t = tarefas.find(function (element, index, array) {
+				return element.id === tarefa.id;
+			});
+			t.done = true;
+		},
+		removeTarefa: function (tarefa) {
+			tarefas.splice(tarefa.id, 1);
+		}
+	}
+})
 
